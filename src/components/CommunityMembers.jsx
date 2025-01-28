d89916761b977388648e88d03c890ccbc717b841
AmullyaPatil <patilamullya@gmail.com>
2025-01-28 17:56:50 +0530

    members modifies

--- src/components/CommunityMembers.jsx
+++ src/components/CommunityMembers.jsx
@@ -1,27 +1,44 @@
 import React from "react";
 import "../styles/CommunityMembers.css";
-import user from "../Assets/image.png";
+import image1 from "../Assets/member1.png";
+import image2 from "../Assets/member2.png";
+import image3 from "../Assets/member3.png";
+
+const members = [
+  {
+    name: "Growth Strategist",
+    role: "A",
+    image:image1,
+  },
+  {
+    name: "Tech Enthusiast",
+    role: "V.",
+    image: image2,
+  },
+  {
+    name: "Product Designer",
+    role: "M",
+    image: image3,
+  },
+];
 
 const CommunityMembers = () => {
-  const members = [
-    { name: "A", role: "Growth Strategist", image:user },
-    { name: "V", role: "Founder & Tech Enthusiast", image:user },
-    { name: "M", role: "Product Designer", image:user },
-    { name: "C", role: "Marketing Expert", image:user },
-  ];
-
   return (
     <div className="community-members">
       <h2 className="community-section-title">Active Community Members</h2>
       <div className="community-members-grid">
         {members.map((member, index) => (
-          <div className="community-member-card" key={index}>
-            <img src={member.image} alt={member.name} className="comm-member-image" />
-            <h3 className="community-member-name">{member.name}</h3>
-            <p className="community-member-role">{member.role}</p>
-            <div className="community-social-icons">
-              <a href="#" className="community-icon">𝕩</a>
-              <a href="#" className="community-icon">✉️</a>
+          <div
+            key={index}
+            className="community-member-card"
+            style={{ backgroundImage: `url(${member.image})` }}
+          >
+            <div className="community-member-header">
+              <h3>{member.name}</h3>
+            </div>
+            <div className="community-member-overlay">
+              <p>{member.role}</p>
+              <button className="community-button">Get in Touch</button>
             </div>
           </div>
         ))}
